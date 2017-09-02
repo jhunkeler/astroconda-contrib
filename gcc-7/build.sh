4d5698f25e02969566aeaded501af59e21d6b70f
Joseph Hunkeler <jhunkeler@gmail.com>
2017-09-02 11:06:13 -0400

    Fixup build

--- gcc-7/build.sh
+++ gcc-7/build.sh
@@ -1,37 +1,34 @@
-topdir=$(pwd)
-objdir=${topdir}/build
-
 mkdir -p $PREFIX/lib
 ln -s $PREFIX/lib $PREFIX/lib64
-ln -s $PREFIX/lib $PREFIX/lib32
 
-mkdir -p ${objdir}
-pushd ${objdir}
+#sed -i.orig -e 's|lib64|lib|g' gcc/config.gcc
+#sed -i.orig -e 's|\.\./lib64|../lib|' \
+#    -e 's|\.\./libx32|../lib|' \
+#    gcc/config/i386/t-linux64 \
+#    gcc/genmultilib
 
-sed -i.orig -e 's|lib64|lib|g' ${topdir}/gcc/config.gcc
-
-${topdir}/configure --prefix=$PREFIX \
+./configure \
+    --prefix=$PREFIX \
     --libdir=$PREFIX/lib \
+    --datadir=$PREFIX/share \
     --with-gmp=$PREFIX \
     --with-mpc=$PREFIX \
     --with-mpfr=$PREFIX \
     --with-isl=$PREFIX \
     --with-cloog=$PREFIX \
     --with-tune=generic \
-    --with-linker-hash-style=gnu \
-    --with-gcc-major-version-only \
     --disable-multilib \
     --disable-bootstrap \
     --disable-libunwind-exceptions \
     --enable-shared \
     --enable-checking=release \
     --enable-threads=posix \
-    --enable-languages=c,c++,fortran,go \
-    --enable-linker-build-id
+    --enable-linker-build-id \
+    --enable-languages=c,c++,fortran
 
 make -j${CPU_COUNT}
-make install
+make install-stripped
 
-popd
 rm -f $PREFIX/lib64
-rm -f $PREFIX/lib32
+[ -e "$PREFIX/bin/cc" ] || ln -s "gcc" "$PREFIX/bin/cc"
+
