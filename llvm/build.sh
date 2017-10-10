c2e53b51df64ba38f7328bb43fc64f0d46d19330
Joseph Hunkeler <jhunkeler@gmail.com>
2017-10-09 20:02:33 -0400

    asdfasdl

--- llvm/build.sh
+++ llvm/build.sh
@@ -22,9 +22,11 @@ if [ ! -x `which $fetch` ]; then
     fi
 fi
 
-release_url=http://llvm.org/releases/${PKG_VERSION}
+#release_url=http://llvm.org/releases/${PKG_VERSION}
+release_branch=release_50
+release_url=https://github.com/llvm-mirror
 packages=(
-    cfe
+    clang
     clang-tools-extra
     compiler-rt
     libcxx
@@ -39,14 +41,16 @@ packages=(
 for p in "${packages[@]}"
 do
     pkg="${p}-${PKG_VERSION}"
-    pkg_tarball="${pkg}.src.tar.xz"
+    #pkg_tarball="${pkg}.src.tar.xz"
     pkg_unpacked="${pkg}.src"
-    $fetch $fetch_args ${release_url}/${pkg_tarball}
+    #$fetch $fetch_args ${release_url}/${pkg_tarball}
+    git clone --depth 1 --branch ${release_branch} --single-branch ${release_url}/${p} ${pkg_unpacked}
 
-    tar xf $pkg_tarball
+    #tar xf $pkg_tarball
+    #rm -f "${pkg_tarball}"
 
     case "$p" in
-        cfe)
+        clang)
             mv $pkg_unpacked $top/tools/clang
         ;;
         clang-tools-extra)
@@ -108,18 +112,24 @@ case "$OS" in
         export LDFLAGS="-L$PREFIX/lib -L/usr/lib64"
 
         cmake \
-            -DCMAKE_INSTALL_PREFIX=${PREFIX} \
             -DCMAKE_BUILD_TYPE=Release \
+            -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
+            -DDEFAULT_SYSROOT="${PREFIX}" \
+            -DGCC_INSTALL_PREFIX="${PREFIX}" \
+            -DCMAKE_C_COMPILER="${PREFIX}/bin/gcc" \
+            -DCMAKE_CXX_COMPILER="${PREFIX}/bin/g++" \
             -DCMAKE_CXX_LINK_FLAGS="-Wl,-rpath,${PREFIX}/lib -L${PREFIX}/lib" \
             -DCURSES_INCLUDE_PATH="${PREFIX}/include" \
             -DFFI_INCLUDE_DIR=`pkg-config libffi --variable=includedir` \
             -DFFI_LIBRARY_DIR=`pkg-config libffi --variable=libdir` \
+            -DBUILD_SHARED_LIBS=yes \
+            -DLLVM_BUILD_STATIC_LIBS=no \
             -DLLVM_TARGETS_TO_BUILD="X86" \
             -DLLVM_ENABLE_FFI=yes \
-            -DLLVM_ENABLE_LTO=full \
+            -DLLVM_ENABLE_LTO=no \
             -DLLVM_ENABLE_LIBCXX=yes \
-            -DLLVM_ENABLE_EH=yes \
-            -DLLVM_ENABLE_RTTI=yes \
+            -DLLVM_ENABLE_EH=no \
+            -DLLVM_ENABLE_RTTI=no \
             -DLLVM_OPTIMIZED_TABLEGEN=yes \
             -DLLDB_DISABLE_PYTHON=1 \
             -DLIBOMPTARGET_SYSTEM_TARGETS="x86_64-pc-linux-gnu" \
